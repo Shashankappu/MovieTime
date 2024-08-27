d1fe2e0b2e0d6d305261e2f9ae4495e6c3e1c32c
Shashankappu <Shashankprince523@gmail.com>
2024-08-27 21:42:14 +0530

    Added TopRatedMoviesList

--- app/src/main/java/com/example/movietime/ui/screens/BottomNavigationBar.kt
+++ app/src/main/java/com/example/movietime/ui/screens/BottomNavigationBar.kt
@@ -21,23 +21,14 @@ fun BottomNavigationBar(navController: NavHostController) {
         backgroundColor = bgPurple
     ) {
 
-        // observe the backstack
         val navBackStackEntry by navController.currentBackStackEntryAsState()
 
-        // observe current route to change the icon
-        // color,label color when navigated
         val currentRoute = navBackStackEntry?.destination?.route
 
-        // Bottom nav items we declared
         Constants.BottomNavItems.forEach { navItem ->
             val isSelected = currentRoute == navItem.route
-            // Place the bottom nav items
             BottomNavigationItem(
-
-                // it currentRoute is equal then its selected route
                 selected = isSelected,
-
-                // navigate on click
                 onClick = {
                     navController.navigate(navItem.route) {
                         // Pop up to the start destination to avoid building up a large backstack
@@ -46,14 +37,10 @@ fun BottomNavigationBar(navController: NavHostController) {
                                 saveState = true
                             }
                         }
-                        // Avoid multiple copies of the same destination when reselecting the same item
                         launchSingleTop = true
-                        // Restore state when reselecting a previously selected item
                         restoreState = true
                     }
                 },
-
-                // Icon of navItem
                 icon = {
                     Icon(
                         painter = painterResource(id = navItem.icon),
@@ -61,8 +48,6 @@ fun BottomNavigationBar(navController: NavHostController) {
                         tint =  if (isSelected) orange else Color.Gray.copy(alpha = 0.3f)
                     )
                 },
-
-                // label
                 label = {
                     Text(text = navItem.label)
                 },
--- app/src/main/java/com/example/movietime/utils/Constants.kt
+++ app/src/main/java/com/example/movietime/utils/Constants.kt
@@ -5,7 +5,6 @@ import androidx.annotation.RequiresApi
 import com.example.movietime.R
 import com.example.movietime.model.Genre
 import com.example.movietime.model.Movie
-import java.time.LocalDate
 
 object Constants {
     val BottomNavItems = listOf(
@@ -32,7 +31,7 @@ val dummyMovies = listOf(
     Movie(
         id = 1,
         title = "The Godfather",
-        rating = 8.5,
+        voteAverage = 8.5,
         voteCount = 1200000,
         summary = "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
         runtime = 175,
@@ -40,14 +39,14 @@ val dummyMovies = listOf(
         genres = listOf(Genre(1, "Crime"), Genre(2, "Drama"),Genre(4, "Adventure"),Genre(5, "History")),
         adult = true,
         imageUrl = "https://imgs.search.brave.com/pbA7p0uoB5HP1uEe2EOwTAOsNE-Coe6-8xqcbhOWBA8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL00v/TVY1Qk0yTXlOall4/Tm1VdFlUQXdOaTAw/TVRZeExXSm1OV1l0/WXpabE9EWTNaVGsz/T1RGbFhrRXlYa0Zx/Y0dkZVFYVnlOemt3/TWpRNU56TUAuanBn",
-        releaseDate = LocalDate.of(1983, 3, 24),
+        releaseDate ="1983",
         tagline = "An offer you can't refuse.",
         trailerUrl = "https://example.com/godfather-trailer.mp4"
     ),
     Movie(
         id = 2,
         title = "Star Wars",
-        rating = 7.0,
+        voteAverage = 7.0,
         voteCount = 2200000,
         summary = "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee, and two droids to save the galaxy from the Empire.",
         runtime = 121,
@@ -55,14 +54,14 @@ val dummyMovies = listOf(
         genres = listOf(Genre(3, "Action"), Genre(4, "Adventure")),
         adult = false,
         imageUrl = "https://c4.wallpaperflare.com/wallpaper/153/860/948/movie-poster-star-wars-star-wars-the-rise-of-skywalker-2019-year-movies-hd-wallpaper-preview.jpg",
-        releaseDate = LocalDate.of(2010, 5, 25),
+        releaseDate = "2010, 5, 25",
         tagline = "A long time ago in a galaxy far, far away...",
         trailerUrl = "https://example.com/star_wars-trailer.mp4"
     ),
     Movie(
         id = 3,
         title = "The Mongol",
-        rating = 6.0,
+        voteAverage = 6.0,
         voteCount = 850000,
         summary = "The story of Genghis Khan's rise to power.",
         runtime = 120,
@@ -70,14 +69,14 @@ val dummyMovies = listOf(
         genres = listOf(Genre(5, "History"), Genre(2, "Drama")),
         adult = true,
         imageUrl = "https://m.media-amazon.com/images/M/MV5BODlhNTk4ODYtOThlMy00N2E1LThkZDktZjZlMjE1YjM3YTgwL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg",
-        releaseDate = LocalDate.of(2001, 9, 15),
+        releaseDate ="2001, 9, 15",
         tagline = "The Untold Story of the Great Conqueror.",
         trailerUrl = "https://example.com/mongol-trailer.mp4"
     ),
     Movie(
         id = 4,
         title = "Thor",
-        rating = 8.0,
+        voteAverage = 8.0,
         voteCount = 1700000,
         summary = "The powerful but arrogant god Thor is cast out of Asgard to live amongst humans in Midgard (Earth), where he soon becomes one of their finest defenders.",
         runtime = 115,
@@ -85,14 +84,14 @@ val dummyMovies = listOf(
         genres = listOf(Genre(3, "Action"), Genre(6, "Fantasy")),
         adult = false,
         imageUrl = "https://m.media-amazon.com/images/M/MV5BOGE4NzU1YTAtNzA3Mi00ZTA2LTg2YmYtMDJmMThiMjlkYjg2XkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_.jpg",
-        releaseDate = LocalDate.of(2019, 5, 2),
+        releaseDate ="2019, 5 2",
         tagline = "The God of Thunder.",
         trailerUrl = "https://example.com/thor-trailer.mp4"
     ),
     Movie(
         id = 5,
         title = "Avatar",
-        rating = 6.0,
+        voteAverage = 6.0,
         voteCount = 1800000,
         summary = "A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
         runtime = 162,
@@ -100,14 +99,14 @@ val dummyMovies = listOf(
         genres = listOf(Genre(7, "Sci-Fi"), Genre(4, "Adventure")),
         adult = false,
         imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyEW3ADcZeKxOOjEXPy6rDYmq4kulo-R6tkg&s",
-        releaseDate = LocalDate.of(2016, 12, 18),
+        releaseDate = "2016, 12, 18",
         tagline = "Enter the World of Pandora.",
         trailerUrl = "https://example.com/avatar-trailer.mp4"
     ),
     Movie(
         id = 6,
         title = "Fire",
-        rating = 9.0,
+        voteAverage = 9.0,
         voteCount = 950000,
         summary = "A gripping story of survival and hope against all odds.",
         runtime = 130,
@@ -115,7 +114,7 @@ val dummyMovies = listOf(
         genres = listOf(Genre(8, "Drama"), Genre(9, "Thriller")),
         adult = true,
         imageUrl = "https://m.media-amazon.com/images/M/MV5BMjIxNzY5MjIzNV5BMl5BanBnXkFtZTgwNTIwMzgwMzE@._V1_.jpg",
-        releaseDate = LocalDate.of(2000, 8, 20),
+        releaseDate = "2000, 8, 20",
         tagline = "The Flame of Courage.",
         trailerUrl = "https://example.com/fire-trailer.mp4"
     )
